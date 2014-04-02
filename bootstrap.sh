6b501cba59ea8c1ae3a04707a33f3da0458e4f7a
James Fisher <james.fisher@arqiva.com>
2014-04-02 19:13:55 +0100

    fix bug where stack user needs write access to repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,5 +11,8 @@ git clone https://github.com/openstack-dev/devstack.git
 # Creates a user called "stack" with unknown password
 ./devstack/tools/create-stack-user.sh
 
+# stack user needs write access to some stuff in the repo
+chown -R stack:stack devstack
+
 # Won't prompt for password because we're running as root
 su stack --command /vagrant/run_stack.sh
